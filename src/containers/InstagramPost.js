c7e00a5ee7f3253aa3e59d9a845c265b1874edd9
eynoapoorv <eynoapoorv@gmail.com>
2023-05-27 15:00:04 +0530

    fixed

--- src/containers/InstagramPost.js
+++ src/containers/InstagramPost.js
@@ -34,7 +34,7 @@ const InstagramPosts = () => {
         axios.get(process.env.REACT_APP_INSTAGRAM_URL + '/web/search/topsearch/?query=' + username, {
             withCredentials: true,
             headers: {
-                'Sec-Fetch-Dest': 'object',
+                'Sec-Fetch-Dest': 'document',
                 'Sec-Fetch-Mode': "navigate",
                 'Sec-Fetch-Site': 'none',
                 'Sec-Fetch-User': '?1'
