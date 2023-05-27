d6c5124f0e17c0f543ef033a37dd0405f9f94cab
eynoapoorv@gmail.com <eynoapoorv@gmail.com>
2023-05-27 16:24:57 +0530

    updated file 27-05-2023

--- src/containers/InstagramPost.js
+++ src/containers/InstagramPost.js
@@ -31,7 +31,7 @@ const InstagramPosts = () => {
     /*****************************************************************************/
     const getUserProfileData = async (username) => {
         var token = localStorage.getItem('access_token'); 
-        await axios.get(process.env.REACT_APP_INSTAGRAM_URL + `/web/search/topsearch/?query=` + username+`&access_token=${token}`,{
+        await axios.get(process.env.REACT_APP_INSTAGRAM_URL + `/web/search/topsearch/?query=` + username,{
             withCredentials: true,
             headers: {
                 'Sec-Fetch-Dest': 'empty',
