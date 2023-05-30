4d85a3da83e83bda0a8a3391ee2fda5dbda05b4d
eynoapoorv@gmail.com <eynoapoorv@gmail.com>
2023-05-30 19:09:51 +0530

    user controller 30-05-2023

--- backend/app/controllers/user.controller.js
+++ backend/app/controllers/user.controller.js
@@ -36,7 +36,7 @@ module.exports = router;
  */
 function joinCompetition(req, res, next) {
     userService.joinCompetion(req)
-        .then(notification => notification ? (notification && notification.isActive === true ? res.json({ status: true, message: msg.user.login.success, data: notification }) : res.status(400).json({ status: false, message: msg.user.login.active })) : res.status(400).json({ status: false, message: msg.user.login.error }))
+        .then(notification => notification ? (notification && notification.isActive === true ? res.json({ status: true, message: msg.user.login.success, data: notification }) : res.status(400).json({ status: false, message: msg.user.login.active })) : res.status(400).json({ status: false, message: next(err) }))
         .catch(err => next(err));
 }
 
