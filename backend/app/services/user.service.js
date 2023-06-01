53dda718365982e73821d3aacac76c6528eb68b3
eynoapoorv <eynoapoorv@gmail.com>
2023-06-01 11:33:36 +0530

    fixed

--- backend/app/services/user.service.js
+++ backend/app/services/user.service.js
@@ -5,14 +5,13 @@
  * Author: Eynosoft Team
  */
 
-// const config = require('../config/index');
+const config = require('../config/index');
 
-const { connectionString } = require("../config/index");
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
 
-mongoose.connect(process.env.CONNECTION_STRING || connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_STRING || config.connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = global.Promise;
 
 const msg = require('../helpers/messages.json');
