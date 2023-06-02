fd907b47594121cc0497816c98e367a947dffa65
eynoapoorv <eynoapoorv@gmail.com>
2023-06-02 18:18:31 +0530

    fixed

--- backend/app/models/competition.model.js
+++ backend/app/models/competition.model.js
@@ -18,7 +18,6 @@ const schema = new Schema({
         media_Url: {
             type: String,
             required: false,
-            trim: true,
             default: ''
         },
         username: {
