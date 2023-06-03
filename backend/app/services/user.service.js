8164f7b57709738d29f64443bfa67fdb77d61c87
eynoapoorv <eynoapoorv@gmail.com>
2023-06-03 11:32:19 +0530

    fixed

--- backend/app/services/user.service.js
+++ backend/app/services/user.service.js
@@ -111,17 +111,32 @@ async function getUserData(param) {
 async function competationData(req, res) {
     console.log("competation DATA")
     try {
-        const param = req.body;
-        console.log(param)
+        const { firstOpponent, secondOpponent } = req.body
 
-        let input = {
-            firstOpponent: param.uri,
-            secondOpponent: param.uri,
-        };
-        return input;
+        //save
+
+        const item = await new Competition({
+            firstOpponent,
+            secondOpponent
+        }).save();
+        res.status(201).send({
+            success: true,
+            message: "add video",
+            item,
+        });
+
+        // const param = req.body;
+        // console.log(param)
+
+        // let input = {
+        //     firstOpponent: param.uri,
+        //     secondOpponent: param.uri,
+        // // };
+        // return input;
+
         // const Item = new Competition(input);
 
-        // const data = await Item.save();
+        // const data = await Item.save(); 
         // if (data) {
         //     console.log(data);
         //     return true;
